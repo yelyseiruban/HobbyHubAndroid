d0e27b2ead43e3afec5bdef576b5fff9d2e19aad
Dawid <s24805@pjwstk.edu.pl>
2023-12-12 10:23:08 +0100

    added exeptions

--- app/src/main/java/com/yelysei/hobbyharbor/model/Exceptions.kt
+++ app/src/main/java/com/yelysei/hobbyharbor/model/Exceptions.kt
@@ -15,3 +15,16 @@ class InsertUserHobbyException: AppException()
 class NoHobbiesBySpecifiedCategoryName: AppException()
 
 class NoActionsByProgressIdException : AppException()
+
+class AddHobbyNameAlreadyEgsistsException : AppException()
+
+class AddHobbyStartingProgressGreaterThanDesiredException : AppException()
+
+class AddHobbyStartingProgressLowerThanDesiredException : AppException()
+
+/*
+Exepctions handled by input:
+ - new hobby (empty name, to long name, name with illegal chars )
+ - add time to hobby (time below 0, wrong date, starting date is after ending date)
+ - add progress to hobby (below 0, bigger than desired final progress, desired hobby progress to big)
+ */
