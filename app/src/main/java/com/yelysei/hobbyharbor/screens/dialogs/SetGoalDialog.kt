4f441b3ffe66ad89752e7a67e3638830c1b4274e
Yelysei Ruban <ruban.elisey.1996@gmail.com>
2023-12-21 22:31:45 +0100

    material design set goal dialog

--- app/src/main/java/com/yelysei/hobbyharbor/screens/dialogs/SetGoalDialog.kt
+++ app/src/main/java/com/yelysei/hobbyharbor/screens/dialogs/SetGoalDialog.kt
@@ -1,10 +1,10 @@
 package com.yelysei.hobbyharbor.screens.dialogs
 
-import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.Window
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.yelysei.hobbyharbor.R
 import com.yelysei.hobbyharbor.databinding.SetGoalDialogBinding
 import com.yelysei.hobbyharbor.screens.uiactions.UiActions
 import com.yelysei.hobbyharbor.screens.uiactions.UiActionsImpl
@@ -20,21 +20,21 @@ class SetGoalDialog(
         SetGoalDialogBinding.inflate(LayoutInflater.from(context))
     }
     fun show(onSubmitClickListener: OnSubmitClickListener) {
-        val dialog = Dialog(context)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(true)
-        dialog.setContentView(binding.root)
-        binding.goalEditText.setText(previousGoal?.toString() ?: "")
-        binding.submitButton.setOnClickListener {
-            try {
-                val goal = binding.goalEditText.text.toString().toInt()
-                onSubmitClickListener(goal)
+        val editGoal = binding.goal
+        editGoal.setText(previousGoal?.toString() ?: "")
+        MaterialAlertDialogBuilder(context)
+            .setTitle(R.string.please_set_up_your_goal_for_the_hobby)
+            .setView(binding.root)
+            .setPositiveButton("Submit") { dialog, _ ->
+                try {
+                    val goal: Int = editGoal.text.toString().toInt()
+                    onSubmitClickListener(goal)
+                } catch (e: java.lang.NumberFormatException) {
+                    uiActions.toast("Specified goal is not correct")
+                }
                 dialog.dismiss()
-            } catch (e: java.lang.NumberFormatException) {
-                uiActions.toast("Specified goal is not correct")
             }
-        }
-        dialog.show()
+            .show()
     }
 }
 
