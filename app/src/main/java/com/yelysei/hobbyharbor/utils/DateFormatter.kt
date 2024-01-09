6a78682e84a1de6f4070a4a9fa37a15266fc91f1
Yelysei Ruban <ruban.elisey.1996@gmail.com>
2024-01-09 01:29:37 +0100

    take into account time zone

--- app/src/main/java/com/yelysei/hobbyharbor/utils/DateFormatter.kt
+++ app/src/main/java/com/yelysei/hobbyharbor/utils/DateFormatter.kt
@@ -1,8 +1,5 @@
 package com.yelysei.hobbyharbor.utils
 
-import java.text.SimpleDateFormat
-import java.util.Calendar
-
 /**
  * Copied from https://stackoverflow.com/questions/7953725/how-to-convert-milliseconds-to-date-format-in-android
  * Return date in specified format.
@@ -10,17 +7,19 @@ import java.util.Calendar
  * @param dateFormat Date format
  * @return String representing date in specified format
  */
-fun getFormattedDate(milliSeconds: Long, dateFormat: String): String {
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import java.util.TimeZone
 
-    // Create a DateFormatter object for displaying date in specified format.
-
-    val formatter = SimpleDateFormat(dateFormat)
-
-    // Create a calendar object that will convert the date and time value in milliseconds to date.
+fun getFormattedDate(milliSeconds: Long, dateFormat: String, timeZone: TimeZone = TimeZone.getTimeZone("UTC")): String {
+    val formatter = SimpleDateFormat(dateFormat, Locale.getDefault())
+    formatter.timeZone = timeZone
 
     val calendar: Calendar = Calendar.getInstance()
-    calendar.setTimeInMillis(milliSeconds)
-    return formatter.format(calendar.getTime())
+    calendar.timeInMillis = milliSeconds
+
+    return formatter.format(calendar.time)
 }
 
 fun getDate(milliSeconds: Long): String {
